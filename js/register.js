816e82aeb766d41c919dbbb7455376c17d974933
Blaios <blai.ortuno@gmail.com>
2021-02-10 17:12:46 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -4,28 +4,38 @@ var url = 'https://web-jedi.herokuapp.com/users';
 
 
 var user = {
-    Username : $("#username").closest('form').attr('id'),
-    Password : $("#password").closest('form').attr('id'),
-    Email: $("#email").closest('form').attr('id')
+    Username : "",
+    Password : "",
+    Email: ""
 }
 
-console.log(user);
 
-var data = JSON.stringify(user)
-console.log(data)
 
-  $('#sub').on("click", async () => {
-    await axios.post(url, data, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(function (response) {
-        alert('El usuario ha sido registrado correctamente');
-        console.log(response);
-      })
-      .catch(function (error) {
-        alert('oops, parece que algo ha fallado!');
-        console.log(error);
-      })
+window.addEventListener('load', ()=> {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        let myform = document.forms[0]
+        user.Username = myform["username"].value
+        user.Password = myform["password"].value
+        user.Email = myform["email"].value
+        var data = JSON.stringify(user)
+        console.log(data)
+
+        $('#sub').on("click", async () => {
+            await axios.post(url, data, {
+                headers: {
+                'Content-Type': 'application/json'
+                }
+            })
+            .then(function (response) {
+                alert('El usuario ha sido registrado correctamente');
+                console.log(response);
+            })
+            .catch(function (error) {
+                alert('oops, parece que algo ha fallado!');
+                console.log(error);
+            })
+        });
+    });
 });
