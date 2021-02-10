685d9568909cf2229176067fbbd069e436baddf5
Blaios <blai.ortuno@gmail.com>
2021-02-10 16:36:17 +0100

    fix register.js

--- js/register.js
+++ js/register.js
@@ -14,17 +14,19 @@ console.log(user);
 var data = JSON.stringify(user)
 console.log(data)
 
-axios.post(url, data, {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(function (response) {
-    alert('El usuario ha sido registrado correctamente');
-    console.log(response);
-  })
-  .catch(function (error) {
-    alert('oops, parece que algo ha fallado!');
-    console.log(error);
-  })
-;
+
+  $('#sub').on("click", async () => {
+    await axios.post(url, data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(function (response) {
+        alert('El usuario ha sido registrado correctamente');
+        console.log(response);
+      })
+      .catch(function (error) {
+        alert('oops, parece que algo ha fallado!');
+        console.log(error);
+      })
+});
