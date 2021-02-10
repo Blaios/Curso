5ab11ef5e53ec6edb3d65f316446429b241f1427
Blaios <blai.ortuno@gmail.com>
2021-02-10 16:14:36 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,13 +1,24 @@
-let user = {
-    Username : "",
-    Password : "",
-    Email: ""
+
+
+var url = '[https://web-jedi.herokuapp.com/users]';
+
+var user = {
+    Username : $("#username").val(),
+    Password : $("#password").val(),
+    Email: $("#email").val()
 }
 
-axios.get('https://web-jedi.herokuapp.com/users')
-    .then( response => {
-        console.log(response.data)
-    })
-    .catch (error => {
-        console.log(error)
-    })
+axios.post(url, user, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(function (response) {
+    alert('El usuario ha sido registrado correctamente');
+    console.log(response);
+  })
+  .catch(function (error) {
+    alert('oops, parece que algo ha fallado!');
+    console.log(error);
+  })
+;
