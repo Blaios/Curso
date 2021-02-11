874f595a537bc0af888f3f149cdfe241ee8427bb
Blaios <blai.ortuno@gmail.com>
2021-02-11 15:50:01 +0100

    a

--- js/register.js
+++ js/register.js
@@ -4,31 +4,30 @@ var user = {
     username : "",
     email: "",
     password : ""
-    
 }
 
 
 window.addEventListener('load',()=> {
-    const form = document.querySelector('form');
+    const form = document.querySelector('form')
     form.addEventListener('submit', async(e)=>{
         e.preventDefault();
-        let myform = document.forms[0]
-        user.username = myform["username"].value
-        user.password = myform["password"].value
-        user.email = myform["email"].value
+        var form_doc = document.forms[0]
+        user.username = form_doc["username"].value
+        user.password = form_doc["password"].value
+        user.email = form_doc["email"].value
         var data = JSON.stringify(user)
         console.log(data)
 
         await axios.post(url, data)
             .then(function (response) {
-                window.location.replace("file:///C:/Users/blaio/Documents/GitHub/Curso/index.html");
-                alert('El usuario ha sido registrado correctamente');
-                console.log(response);
+                window.location.replace("file:///C:/Users/blaio/Documents/GitHub/Curso/index.html")
+                alert('El usuario ha sido registrado correctamente')
+                console.log(response)
             })
             .catch(function (error) {
                 console.log(data)
-                alert('oops, parece que algo ha fallado!');
-                console.log(error);
+                alert('oops, parece que algo ha fallado!')
+                console.log(error)
             })
     });
 });
