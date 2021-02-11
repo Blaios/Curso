72ba01037bf96bd5a81d51f25e056a4f906ae67e
Blaios <blai.ortuno@gmail.com>
2021-02-11 19:21:43 +0100

    aa

--- js/login.js
+++ js/login.js
@@ -1,39 +1,35 @@
 var url = 'https://web-jedi.herokuapp.com/users';
 
+var id = 1;
+
 
 window.addEventListener('load', async()=> {
     const form = document.querySelector('form')
     form.addEventListener('submit', async(e)=>{
         e.preventDefault();
         var form_doc = document.forms[0]
-        var username = form_doc["username"].value
-        var password = form_doc["password"].value
         try {
-            res = await axios.get(url);
-            login(res, username, password);
+            var res = (await axios.get(url)).data;
+            alert('El usuario ha sido registrado correctamente')
+            console.log(response)
+            var i = 0;
+            console.log(res.length)
+            while (i < res.length) {
+                console.log(res[i])
+                i++;
+            }
         }
         catch(error) {
-            console.log(data)
-            alert('oops, parece que algo ha fallado!')
-            console.log(error)
+            var i = 0;
+            while (i < 4) {
+                console.log(res[i])
+                i++;
+            }
+                alert('oops, parece que algo ha fallado!')
+                console.log(error)
+                console.log(res)
+                console.log(res.length)
+                console.log(res[i].password)
         }
     });
 });
-
-function login(response, username, password) {
-    result = $("#username", response);
-    console.log(result);
-    result2 = $("#password", response);
-    console.log(result2);
-    if (result && result2) {
-        alert('El usuario ha sido registrado correctamente')
-        console.log(response)
-        window.location.replace("https://blaios.netlify.app/home")
-    }
-    else {
-        alert('oops, parece que algo ha fallado!')
-        console.log(error)
-        window.location.replace("https://blaios.netlify.app")
-    }
-   
-}
