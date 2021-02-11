df5917b1b535ac529ec002375a401f60129b4410
Blaios <blai.ortuno@gmail.com>
2021-02-11 18:21:10 +0100

    a

--- js/login.js
+++ js/login.js
@@ -1,33 +1,39 @@
 var url = 'https://web-jedi.herokuapp.com/users';
 
-var user = {
-    username : "",
-    password : ""
-}
 
-window.addEventListener('load',()=> {
+window.addEventListener('load', async()=> {
     const form = document.querySelector('form')
     form.addEventListener('submit', async(e)=>{
         e.preventDefault();
         var form_doc = document.forms[0]
-        user.username = form_doc["username"].value
-        user.password = form_doc["password"].value
-        var data = JSON.stringify(user)
-        console.log(data)
-
-        await axios.get(url, data)
-            .then(function (response) {
-                window.location.replace("https://blaios.netlify.app/home")
-                alert('El usuario ha sido registrado correctamente')
-                console.log(response)
-            })
-            .catch(function (error) {
-                console.log(data)
-                alert('oops, parece que algo ha fallado!')
-                console.log(error)
-            })
+        var username = form_doc["username"].value
+        var password = form_doc["password"].value
+        try {
+            res = await axios.get(url);
+            login(res, username, password);
+        }
+        catch(error) {
+            console.log(data)
+            alert('oops, parece que algo ha fallado!')
+            console.log(error)
+        }
     });
 });
 
-
-function login ()
+function login(response, username, password) {
+    result = $("#username", response);
+    console.log(result);
+    result2 = $("#password", response);
+    console.log(result2);
+    if (result && result2) {
+        alert('El usuario ha sido registrado correctamente')
+        console.log(response)
+        window.location.replace("https://blaios.netlify.app/home")
+    }
+    else {
+        alert('oops, parece que algo ha fallado!')
+        console.log(error)
+        window.location.replace("https://blaios.netlify.app")
+    }
+   
+}
